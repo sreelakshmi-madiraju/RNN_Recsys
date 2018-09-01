eca670014f9c18f064958a66d29f417de2561a36
sreelakshmi-madiraju <40469066+sreelakshmi-madiraju@users.noreply.github.com>
2018-09-01 18:30:29 +0530

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -1,59 +1,74 @@
 from gensim.models import Word2Vec
-from sklearn.preprocessing import LabelEncoder
-import numpy as np
-import _pickle as cPickle
-import pickle
-import hickle as hkl
+ from sklearn.preprocessing import LabelEncoder
+ import numpy as np
+ import _pickle as cPickle
+ np.random.seed(123)
 
-#new_list=np.load('user_purchase_seq.npy')
-#items_pruned contains all user puchase sequences ordered by time.
-#each item is purchased by min 10 users and each user purchased min 10 items
-with open('./items_pruned1.pkl','rb') as f:
-	new_list=pickle.load(f)
-#model = Word2Vec(new_list,size =50,window = 3,min_count =1)
+ # Model for numerical representation of the products
+ def word2vec_model(new_list):
+     model = Word2Vec(new_list,size =50,window = 3,min_count =1)
+     return model
 
-#model.save('word2vec_model')
-model = Word2Vec.load('word2vec_model')
+ # Last item purchased by the user in each sequence is taken as target
+ def get_target_data(new_list):
+     target = []
+     for i in range(0, len(new_list)):
+         q=len(new_list[i])
+         for j in range(0,q):
+             if j+5<q:
+                 target.append(new_list[i][j+5])
 
-entire_products=[]
-for key,value in model.wv.vocab.items():
-		entire_products.append(key)
-#print((entire_products))
+     return target
 
-product_label=LabelEncoder()
-product_label.fit(entire_products)
-num_label=product_label.transform(entire_products)
+ # Numbering every unique product to create one hot for target
+ def num_products(model,new_list):
+     entire_products=[]
+     for key,value in model.wv.vocab.items():
+         entire_products.append(key)
 
-target=[]
-new_list1=[]
-for i in range(0,len(new_list)):
-	new_list1.append(new_list[i])
-	target.append(new_list[i][-1])
-print(len(target))
-print(len(new_list1))
+     print(len(entire_products))
+     product_label=LabelEncoder()
+     product_label.fit(entire_products)
+     target=get_target_data(new_list)
+     target_int = product_label.transform(target)
+     return target_int
 
-new_list=new_list1
-target_int=product_label.transform(target)
-print(len(target_int))
+ #Creating five length sequences
+ def five_len_sequences(new_listy):
+     with open('./user_item_seq','rb') as f:
+         user_item=cPickle.load(f)
+     with open('./user_embed','rb') as f:
+         user_embed=cPickle.load(f)
 
-n_values = np.max(target_int) + 1
-print(n_values,target_int)
-y_tot=np.zeros((len(new_list),len(entire_products)))
-print(np.shape(y_tot))
-for i in range(0,len(new_list)):
-	y_tot[i][target_int[i]]=1
+     user_input=[]
+     tot_x=[]
+     for i in range(0,len(new_listy)):
+         q = len(new_listy[i])
+         for k,v in user_item.items():
+             if [new_listy[i]]==v:
+                 user=k
+         for j in range(0,q):
+             if j+5<q:
+                 tot_x.append(new_listy[i][j:j+5])
+                 user_input.append(user_embed[user])
+                 
+     with open('./user_input.pkl','wb')as f:
+         cPickle.dump(user_input,f)
+     return tot_x
 
-#y_tot=np.eye(n_values)[target_int]
 
 
-for i in range(0,len(new_list)):
-	q=len(new_list[i])
-	new_list[i]=new_list[i][q-10:q-1]
+ new_list=np.load('user_purchase_seq.npy')
+ target_data=get_target_data(new_list)
+ np.save('target_data.npy',target_data)
 
-print("Word2vec model built.")
-print("Labelled sequences and One hot for target data created")
-with open('tot_x1.pkl','wb') as f:
-   cPickle.dump(new_list,f)
-#with open('tot_y1.pkl','wb') as f:
- #  cPickle.dump(y_tot,f,protocol=2)
-np.save('tot_y.npy',y_tot)
+ model=word2vec_model(new_list)
+ #model.save('word2vec_model')
+ y_seq=num_products(model,new_list)
+ x_seq=five_len_sequences(new_list)
+ print("Word2vec model built.")
+ print("Labelled target sequences")
+
+
+ np.save('tot_x_seq1.npy',x_seq)
+ np.save('tot_y1.npy',y_seq)
